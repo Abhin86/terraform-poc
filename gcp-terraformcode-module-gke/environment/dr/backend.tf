cd5916a16cb2e033cf5a0d6fbfbc1e7952f7ed8e
Abhin86 <124777743+Abhin86@users.noreply.github.com>
2023-08-16 15:38:48 +0530

    Update backend.tf

--- gcp-terraformcode-module-gke/environment/dr/backend.tf
+++ gcp-terraformcode-module-gke/environment/dr/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket = "disaster-recovery-poc"
     prefix = "tfstate"
-    credentials = "/home/achuabhin86/cred.json"
+#    credentials = "/home/achuabhin86/cred.json"
   }
 }
