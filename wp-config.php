eca52b71ec6e3bfae7afb19e0fdb7216287cca81
SITHARA MANEESH <91305911+sitharamaneesh@users.noreply.github.com>
2023-08-24 10:32:09 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'wordpress' );
 
 /** Database hostname */
-define( 'DB_HOST', '${{ secrets.DB1 }}'  );
+define( 'DB_HOST', $DB1  );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
