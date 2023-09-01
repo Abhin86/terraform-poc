537d4acd6b38d6308747365fa8bb8b36b0b82c95
Abhin86 <124777743+Abhin86@users.noreply.github.com>
2023-09-01 12:48:29 +0530

    Update main.tf

--- gcp-terraformcode-module-gke/environment/prod/main.tf
+++ gcp-terraformcode-module-gke/environment/prod/main.tf
@@ -16,10 +16,10 @@ module "gke" {
   cluster_name = var.cluster_name
   network = module.vpc.network_name
   subnetwork = module.vpc.subnet_name
-  node_count = var.node_count
-  machine_type = var.machine_type
-  oauth_scopes = var.oauth_scopes
-  node_pool_name = var.node_pool_name
+ # node_count = var.node_count
+# # machine_type = var.machine_type
+#  oauth_scopes = var.oauth_scopes
+#  node_pool_name = var.node_pool_name
   enable_private_nodes = var.enable_private_nodes
   enable_private_endpoint = var.enable_private_endpoint
   master_ipv4_cidr_block = var.master_ipv4_cidr_block
@@ -27,6 +27,30 @@ module "gke" {
   services_ipv4_cidr_block = var.services_ipv4_cidr_block
   disk_size_gb = var.disk_size_gb
 }
+#
+resource "google_container_node_pool" "primary_nodes" {
+  name       = var.node_pool_name
+  location   = module.gke.cluster_location
+  cluster    = module.gke.cluster-name
+  node_count = var.node_count
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+    labels = {
+      env = "dev"
+    }
+    machine_type = var.machine_type
+    disk_size_gb   = var.disk_size_gb
+    preemptible  = true
+       metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+#
+
 # create cloud router for nat gateway
 resource "google_compute_router" "router" {
   project = var.project
