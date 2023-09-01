c4cc200fd88a3b4a31bc4adcca1879896a2935a6
SITHARA MANEESH <91305911+sitharamaneesh@users.noreply.github.com>
2023-09-01 12:54:34 +0530

    Update main.tf

--- gcp-terraformcode-module-gke/environment/prod/main.tf
+++ gcp-terraformcode-module-gke/environment/prod/main.tf
@@ -16,10 +16,10 @@ module "gke" {
   cluster_name = var.cluster_name
   network = module.vpc.network_name
   subnetwork = module.vpc.subnet_name
- # node_count = var.node_count
-# # machine_type = var.machine_type
-#  oauth_scopes = var.oauth_scopes
-#  node_pool_name = var.node_pool_name
+  node_count = var.node_count
+  machine_type = var.machine_type
+  oauth_scopes = var.oauth_scopes
+ node_pool_name = var.node_pool_name
   enable_private_nodes = var.enable_private_nodes
   enable_private_endpoint = var.enable_private_endpoint
   master_ipv4_cidr_block = var.master_ipv4_cidr_block
